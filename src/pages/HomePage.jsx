af4665f5a7ce138ffda58e01b33b8be92db17b4e
Parin Jengjaibun <67160402@go.buu.ac.th>
2026-03-27 19:29:51 +0700

    Fix HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
 import PostList from "../components/PostList";
 import AddPostForm from "../components/AddPostForm";
+import PostCount from "../components/PostCount";
+import { useState } from "react";
 
 function HomePage() {
+  const [posts, setPosts] = useState([]);
+
+  function handleAddPost(newPost) {
+    setPosts([newPost, ...posts]);
+  }
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto", padding: "0 1rem" }}>
-      <AddPostForm onAddPost={() => {}} />
-      <PostList />
+      <AddPostForm onAddPost={handleAddPost} />
+      <PostCount count={posts.length} />
+      <PostList posts={posts} />
     </div>
   );
 }
